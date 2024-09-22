5e8e6f51ec4fb26a29a639aed18ad3b96ec4737d
Sachin <leavethesq@gmail.com>
2024-09-23 03:13:27 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav>
+    <h1>Music Player</h1>
+    <span>
+    <RouterLink to="/">Home</RouterLink>
+  </span>
+  </nav>
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HomeViewVue from './views/HomeView.vue'
+</script>
+
+<style>
+nav{
+  background-color: rgb(93, 245, 194);
+  display: flex;
+  text-shadow: 2px 2px black;
+  height: 5rem;
+  align-items: center;
+  margin-bottom: 2em;
+}
+body{
+  background-color: rgb(119, 219, 186);
+  color: snow;
+  
+}
+span{
+  right: 2em;
+  position: absolute;
+  text-shadow: none;
+  font-size: 1.5em;
+}
+nav>h1{
+  left: 3em;
+  position: relative;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="songs">
+    <div class="albumPic">Album Pic</div>
+    <div class="details">
+      <h3>Title</h3>
+      <p>author</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { walkIdentifiers } from "vue/compiler-sfc";
+
+async function song() {
+  let res = await fetch('https://saavn.dev/modules?language=english',{method:'GET'})
+  let data =await res.json();
+  console.log(data); 
+}
+
+song()
+</script>
+
+<style scoped>
+.songs {
+  min-width: 90%;
+  background-color: rgb(6, 194, 106);
+  margin: 4px auto;
+  border-radius: 12px;
+  display: flex;
+}
+
+.albumPic {
+  height: 100px;
+  width: 100px;
+  background-color: rgb(207, 199, 189);
+  border-radius: 12px;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+}
+.details{
+  margin: 1.5em;
+}
+</style>
